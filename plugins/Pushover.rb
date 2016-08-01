77eccdacd4971c8fa947ebead36b0442b8c6750f
cdriediger <christoph@driediger.eu>
2016-08-01 15:17:48 +0200

    Pushover: use_ssl wasn't implementet corrent

--- plugins/Pushover.rb
+++ plugins/Pushover.rb
@@ -22,12 +22,16 @@ module Pushover
     $Log.error("No Token found in Pushover config") unless @config.has_key?('token')
     $Log.error("No User found in Pushover config") unless @config.has_key?('user')
     if @config.has_key?('url')
-      url = URI.parse(@config['url']
+      url = URI.parse(@config['url'])
     else
       if @config.has_key?('use_ssl')
-        url = URI.parse("https://api.pushover.net/1/messages.json")
+        if @config['use_ssl']
+          url = URI.parse("https://api.pushover.net/1/messages.json")
+        else
+          url = URI.parse("http://api.pushover.net/1/messages.json")
+        end
       else
-        url = URI.parse("http://api.pushover.net/1/messages.json")
+        url = URI.parse("https://api.pushover.net/1/messages.json")
       end
     end
    	req = Net::HTTP::Post.new(url.path)
@@ -37,7 +41,7 @@ module Pushover
       :message => data,
 	})
     res = Net::HTTP.new(url.host, url.port)
-    res.use_ssl = url.ssl?
+    res.use_ssl = url.https?
     res.verify_mode = OpenSSL::SSL::VERIFY_PEER
     res.start {|http| http.request(req) }
   end
