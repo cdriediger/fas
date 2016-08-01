b9304e0fa3297b9f8028bc6d6b9ebbefec0af692
cdriediger <christoph@driediger.eu>
2016-08-01 11:41:47 +0200

    fixed Whitespace

--- plugins/Email.rb
+++ plugins/Email.rb
@@ -2,20 +2,20 @@ require 'net/smtp'
 
 module Email
 
-    def init_plugin(mode)
-        @name = "Email"
-        @comment = "Send and Recive Emails"
+  def init_plugin(mode)
+    @name = "Email"
+    @comment = "Send and Recive Emails"
 	@signals = {'send_email'=>self.method(:send_email)}
-        @jobs = []
-    end
+    @jobs = []
+  end
 
-    def send_email(id, payload, arguments=false)
-        now = Time.now
-        unless arguments
-          puts "Email.send_email needs Arguments"
-          return
-        end
-        msgstr = <<END_OF_MESSAGE
+  def send_email(id, payload, arguments=false)
+    now = Time.now
+    unless arguments
+      puts "Email.send_email needs Arguments"
+      return
+    end
+    msgstr = <<END_OF_MESSAGE
 From: Automation system <automation@driediger.eu>
 To: <#{arguments['email_address']}>
 Subject: #{arguments['email_subject']}
@@ -29,11 +29,11 @@ Arguments:
 End of Payload
 END_OF_MESSAGE
 
-        Net::SMTP.start(@config['smtp_server'], @config['smtp_port']) do |smtp|
-            smtp.send_message msgstr,
-                    arguments['email_address'],
-                    arguments['email_address']
-        end
+    Net::SMTP.start(@config['smtp_server'], @config['smtp_port']) do |smtp|
+      smtp.send_message msgstr,
+      arguments['email_address'],
+      arguments['email_address']
     end
+  end
 
 end
