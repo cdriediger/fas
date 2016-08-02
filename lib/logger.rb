5fcfac8a732b814d4dd0578ff1e6dbf5ef40f828
cdriediger <christoph@driediger.eu>
2016-08-02 16:28:39 +0200

    Debug files now have the .debug file extention

--- lib/logger.rb
+++ lib/logger.rb
@@ -7,9 +7,9 @@ class Log
     logfiles.map! {|path| /\d{1,3}/.match(path).to_s.to_i}
     logfiles.map! {|number| number.to_i}
     if logfiles.empty?
-      @Logfile = path + ".debug.1"
+      @Logfile = path + ".1.debug"
     else
-      @Logfile = path + ".debug." + (logfiles.sort[-1] + 1).to_s
+      @Logfile = path + (logfiles.sort[-1] + 1).to_s  + ".debug."
     end
     puts "Logfile: #{@Logfile}"
     @logger = Logger.new(@Logfile)
