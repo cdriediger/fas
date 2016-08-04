598bdceef673f6b2536e3f121799b4a80c298472
cdriediger <christoph@driediger.eu>
2016-08-04 11:49:54 +0200

    Fas_Logger: Added argument: log_level

--- plugins/Fas_Logger.rb
+++ plugins/Fas_Logger.rb
@@ -5,8 +5,16 @@ module Fas_Logger
     @comment = "Plugins loggs payloads routed to it"
   end
 
-  def info(ip, data, arguments=nil)
-    $Log.info("Fas_Logger: IP: #{ip}, DATA: #{data}, ARGUMENTS: #{arguments}")
+  def info(ip, data, arguments={})
+    if arguments.empty?
+      $Log.info("Fas_Logger: IP: #{ip}, DATA: #{data}, ARGUMENTS: #{arguments}")
+    else  
+      if arguments['log_level'] == 'info'
+        $Log.info("Fas_Logger: IP: #{ip}, DATA: #{data}, ARGUMENTS: #{arguments}")
+      elsif arguments['log_level'] == 'error'
+        $Log.error("Fas_Logger: IP: #{ip}, DATA: #{data}, ARGUMENTS: #{arguments}")
+      end
+    end
   end
 
 end
