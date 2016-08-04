22d45499f289412ab5ef1051577da1cf6695339d
cdriediger <christoph@driediger.eu>
2016-08-04 11:22:46 +0200

    Fas_Logger: Use Global Logger instead of puts

--- plugins/Fas_Logger.rb
+++ plugins/Fas_Logger.rb
@@ -1,12 +1,12 @@
 module Fas_Logger
 
   def init_plugin(mode)
-    @name = "Fas_Logger"  #Change Me
-    @comment = "Plugins loggs payloads routed to it"  #Change me to
+    @name = "Fas_Logger"
+    @comment = "Plugins loggs payloads routed to it"
   end
 
   def info(ip, data, arguments=nil)
-    puts("INFO: #{data}")
+    $Log.info("Fas_Logger: IP: #{ip}, DATA: #{data}, ARGUMENTS: #{arguments}")
   end
 
 end
